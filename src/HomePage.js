60b3794f926b929f4585c847bd07e47d9e91e3f7
SaiMahith Chigurupati <chigurupati.sa@northeastern.edu>
2023-01-01 10:20:36 -0500

    chat dapp

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,49 +1,108 @@
 //Homepage.js
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Chat from "./Chat.js";
 import "./HomePage.css";
+import {ethers} from "ethers";
+import ABI from './ABI.json';
+
 function Homepage(props) {
 
-
     const [message, setMessage] = useState("");
+    const [allChats, setAllChats] = useState([]);
+    const [pagination, setPagination] = useState([0]);
+    const [NFT, setNFT] = useState(0);
+    const [NFTList, setNFTList] = useState([]);
+
+    const sendMessage = async() =>{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x2823FcA3Bf738F7ab879B809ec03BD40819260Ea", ABI, signer);
+        
+        const tryToSend = await contract.addMessage(message,NFT);
+        
+    }
+
+   
+
+    const getMessages = async () =>{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x2823FcA3Bf738F7ab879B809ec03BD40819260Ea", ABI, signer);
+        
+        const currentAddress = provider.getSigner().getAddress();
+        const amountOfNFTs = await contract.balanceOf(currentAddress);
+        //console.log(amountOfNFTs);
+
+        for(var i=0;i<amountOfNFTs;i++){
+            var currentNFT = await contract.tokenOfOwnerByIndex(currentAddress,i);
+            setNFTList(oldNFT =>[...oldNFT,currentNFT]);
+            //console.log(currentNFT);
+
+        }
+        const totalMessages = await contract.totalMessages();
+        const page = 2;
+
+        const starting = totalMessages - (page * pagination)-1;
+        setAllChats([]);
+        for(var i=starting; i> starting - page; i--){
+        
+            if(i>=0){
+            const currentMessage = await contract.messages(i);
+            console.log(currentMessage);
+            setAllChats(prevChats =>[...prevChats,currentMessage]);
+            }
+            
+        }
+        
+    }
+
+    useEffect(()=>{
+        getMessages();
+    },[pagination]);
+
+    const back = async() =>{
+        setPagination(old => old + 1);
+        //getMessages();
+    }
+
+    const forward = async() =>{
+        if(pagination!=0)
+        setPagination(old => old - 1);
+        //getMessages();
+    }
 
     return (
         
-        
+                <section>
+                  <div className="hero">
 
-        <section>
-                  <div class="hero">
+                  <button className="header-cta"><a href="#" onClick={back}>Back</a></button>
+                  <button className="header-cta"><a href="#" onClick={forward}>Forward</a></button>
 
-                      <div className="chatMessage">
-
-
-                          <Chat text="YO" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
-                          <Chat text="Chat message" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
-                          <Chat text="This is a test" image="https://yt3.ggpht.com/ytc/AMLnZu-2DrkobCQd6ri63wO9SuMFGyTbyMhD5kQ6Up2N=s900-c-k-c0x00ffffff-no-rj" data="Oct 20, 2022 8:12PM" />
-                    </div>
-
+                        <div className="chatMessage">
+                            {allChats.map((item=>(<Chat text={item.sentMessage} data = {item.sender +":"+ item.nftID}/>)))}
+                        </div>
 
                       <div className="Send">
+                        <select name = "NFT" id="nftID" onChange={(e)=>setNFT(e.target.value)}>
+                            {NFTList.map((item)=>
+                             (<option key={item.toString()} value={item.toString()}>{item.toString()}</option>))};
+                            
+                            
+                        </select>
                           <input
                               className="textInput"
                               type="text"
                               value={message}
                               onChange={(e) => setMessage(e.target.value)}
                           />
-                          <button class="header-cta"><a href="">Send</a></button>
+                          <button className="header-cta"><a href="#" onClick={sendMessage}>Send</a></button>
                       </div>
-
-
-
-                      
-                      
+    
                   </div>
                 </section> 
-             
 
     );
-
-
 
 }
 
--- src/Mint.js
+++ src/Mint.js
@@ -1,9 +1,21 @@
 import './mint.css';
+import {ethers} from "ethers";
+import ABI from './ABI.json';
 
 function Mint(props){
+
+    const mint = async() =>{
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        provider.send("eth_requestAccounts")
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract("0x2823FcA3Bf738F7ab879B809ec03BD40819260Ea", ABI, signer);
+        const price = await contract.price();
+
+        const tryToMint = await contract.safeMint({value: price});
+    }
     return(
         <div>
-            <button class="mint-button"><a href="">Send</a></button>
+            <button class="mint-button"><a href="#" onClick={mint}>Mint</a></button>
         </div>
     );
 }
